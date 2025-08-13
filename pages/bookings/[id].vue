2c6e93baf1502752d30f16e6250b9e4fb395d9b1
Pedro Gustavo Belarmino <pedro.gbelarmino@gmail.com>
2025-08-13 10:32:57 -0300

    label em ingles

--- pages/bookings/[id].vue
+++ pages/bookings/[id].vue
@@ -66,52 +66,48 @@ function bookingStatus(b: s_reser) {
       </div>
       
       <div class="card mb-4">
-        <div class="card-header bg-secondary text-white">Guest Information</div>
+        <div class="card-header bg-secondary text-white">GUEST INFORMATION</div>
         <div class="card-body">
           <p><strong>Identification:</strong> {{ data.GUEST_R }}</p>
           <p><strong>Inviteds:</strong> {{ data.NUMBER }}</p>
-          <p><strong>Unidades Reservadas:</strong> {{ data.COUNT }}</p>
-          <p><strong>titulo da reserva:</strong> {{ data.MATCH1 }}</p>
+          <p><strong>Reserved Units:</strong> {{ data.COUNT }}</p>
+          <p><strong>Reservatio Title:</strong> {{ data.MATCH1 }}</p>
         </div>
       </div>
 
       <div class="card mb-4">
-        <div class="card-header bg-success text-white">INFORMAÇÔES FINANCEIRAS</div>
+        <div class="card-header bg-success text-white">FIANCIAL INFORMATION</div>
         <div class="card-body">
-          <p><strong>valor total da reserva:</strong> {{ data.PINAD }} {{ data.PINAD1C }}</p>
-          <p><strong>Crédito aberto:</strong> {{ data.CREDITOPEN }}</p>
-          <p><strong>data limite de pagamento:</strong> {{ formatDate(data.CREDITDUE) }}</p>
-          <p><strong>valor incluso:</strong> {{ data.INCPRICE }} {{data.INCPRICEC}}</p>
+          <p><strong>Total Reservation Value:</strong> {{ data.PINAD }} {{ data.PINAD1C }}</p>
+          <p><strong>Open Credit:</strong> {{ data.CREDITOPEN }}</p>
+          <p><strong>Payment Due Date:</strong> {{ formatDate(data.CREDITDUE) }}</p>
+          <p><strong>Included Value:</strong> {{ data.INCPRICE }} {{data.INCPRICEC}}</p>
         </div>
       </div>
 
       <div class="card mb-4">
-        <div class="card-header bg-secondary text-white">OBSERVAÇÕES E ANOTAÇÕES</div>
+        <div class="card-header bg-secondary text-white">NOTES AND COMMENTS</div>
         <div class="card-body">
-          <p><strong>observações internas:</strong> {{ data.MEMO }}</p>
-          <p><strong>texto adicionais:</strong> {{ data.TEXT }}</p>
+          <p><strong>Internal Notes:</strong> {{ data.MEMO }}</p>
+          <p><strong>Additional Text:</strong> {{ data.TEXT }}</p>
         </div>
       </div>
 
       <div class="card mb-4">
-        <div class="card-header bg-success text-white">STATUS E CONTROLE</div>
+        <div class="card-header bg-success text-white">STATUS AND CONTROL</div>
         <div class="card-body">
-          <p><strong>CONFIRMADO:</strong> {{ formatStatus(data.ST_CONF) }}</p>
-          <p><strong>data de confirmação:</strong> {{ formatDate(data.ST_CONFD) }}</p>
-          <p><strong>cancelado:</strong> {{ formatYesNo(data.ST_CANCEL) }}</p>
-          <p><strong>cancelado data:</strong> {{ formatDate(data.CANCELD) }}</p>
-          <p><strong>ST_EXPIRE:</strong> {{ formatYesNo(data.ST_EXPIRE) }}</p>
+          <p><strong>Confirmed:</strong> {{ formatStatus(data.ST_CONF) }}</p>
+          <p><strong>COnfirmation Date:</strong> {{ formatDate(data.ST_CONFD) }}</p>
+          <p><strong>Cancelled:</strong> {{ formatYesNo(data.ST_CANCEL) }}</p>
+          <p><strong>Cancellation Date:</strong> {{ formatDate(data.CANCELD) }}</p>
+          <p><strong>Expired?:</strong> {{ formatYesNo(data.ST_EXPIRE) }}</p>
         </div>
       </div>
 
       <div class="card mb-4">
-        <div class="card-header bg-success text-white">ALIMENTAÇÃO</div>
+        <div class="card-header bg-success text-white">MEAL PLAN</div>
         <div class="card-body">
-          <p><strong>tipo de alimentação??:</strong> {{ data.BOARDSTORE }}</p>
-          <p><strong>data de confirmação:</strong> {{ formatDate(data.ST_CONFD) }}</p>
-          <p><strong>cancelado:</strong> {{ formatYesNo(data.ST_CANCEL) }}</p>
-          <p><strong>cancelado data:</strong> {{ formatDate(data.CANCELD) }}</p>
-          <p><strong>ST_EXPIRE:</strong> {{ formatYesNo(data.ST_EXPIRE) }}</p>
+          <p><strong>Meal Plan Type:</strong> {{ data.BOARDSTORE }}</p>
         </div>
       </div>
 
